0d12e52af4d244e333f2f3177b9e6075f2725232
games235 <games235@skm.vn>
2023-10-16 13:42:15 +0000

    update

--- html5game/vph_API_Fullscreen.js
+++ html5game/vph_API_Fullscreen.js
@@ -38,7 +38,7 @@
 
       ],
       [
-        'mozRequestFullScreen',mut
+        'mozRequestFullScreen',
         'mozCancelFullScreen',
         'mozFullScreenElement',
         'mozFullScreenEnabled',
